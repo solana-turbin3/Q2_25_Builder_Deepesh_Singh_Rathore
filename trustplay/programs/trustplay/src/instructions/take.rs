203971d9105c6d736ff91c20a1ce3e588bb52601
deepesh-sr <220714100078@cutm.ac.in>
2025-05-13 16:34:16 +0530

    instructions updated :)

--- trustplay/programs/trustplay/src/instructions/take.rs
+++ trustplay/programs/trustplay/src/instructions/take.rs
@@ -15,126 +15,100 @@ use crate::state::EscrowState;
 
 
 #[derive(Accounts)]
-pub struct Take<'info> {
+pub struct Release<'info> {
     #[account(mut)]
-    pub taker: Signer<'info>,
-    pub maker: SystemAccount<'info>,
-    #[account(address = escrow.mint_a)] // this constaints prevent abuse of the mint as was provided during the escrow creation process
-    // we would need the mint again here since
-    pub mint_a: InterfaceAccount<'info, Mint>,
-    // #[account(address = escrow.mint_b)] // this constaints prevent abuse of the mint as was provided during the escrow creation process
-    // pub mint_b: InterfaceAccount<'info, Mint>>, 
-    #[account(
-        init_if_needed,
-        payer = taker,
-        associated_token::mint = mint_a, // ? how does this access works associated_token::mint = mint_a
-        associated_token::authority = taker,
-        associated_token::token_program = token_program,
-    )]
-    // this is needed to store the tokens that would be received from 
-    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
-    // #[account(
-    //     mut,  // this is expected to already exist for the taker since they must own those tokens
-    //     associated_token::mint = mint_b, // ? how does this access works associated_token::mint = mint_a
-    //     associated_token::authority = taker,
-    //     associated_token::token_program = token_program,
-    // )]
-    // // this is needed to store the tokens that would be give in the escrow from the take
-    // pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
-    #[account(
-        init_if_needed,
-        payer = taker,
-        // the constaints below are called SPL Constraints, and they are either used as checked or as initialization values
-        // they are used as checks when the account is not init, and vice versa
-        associated_token::mint = mint_b, // ? how does this access works associated_token::mint = mint_a
-        associated_token::authority = maker ,// we can not use escrow.maker, // difference between account and public keys as used in different part of anchor
-        associated_token::token_program = token_program,
-    )] 
-    pub maker_ata_b: InterfaceAccount<'info, TokenAccount>, 
-    #[account(
-        mut,
-        close = taker, 
-        // this checks that the escrow account has a field call mint_b and that field' value == mint_b value
-        // escrow.mint_b == mint_b (from the top of the Account struct)
-        has_one = mint_a, // same check as above
+    pub taker: Signer<'info>,               // remains payer
+    #[account(mut, close = maker,
         has_one = maker,
+        has_one = receiver,
         seeds = [b"escrow", escrow.maker.as_ref(), escrow.seed.to_le_bytes().as_ref()],
-        bump = escrow.bump,
-    )]
+        bump = escrow.bump)]
     pub escrow: Account<'info, EscrowState>,
+
+    /// The account that was set earlier
+    pub receiver: Signer<'info>,
+
+    /// Init the ATA for the receiver if needed
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_a,
+        associated_token::authority = receiver,
+        token_program = token_program,
+        associated_token_program = associated_token_program,
+    )]
+    pub receiver_ata: Account<'info, TokenAccount>,
+
+    /// Vault holding tokens
     #[account(
         mut,
-        associated_token::mint = mint_a, // the constraint here applies to associated_token
-        // vault.mint == mint_a
+        associated_token::mint = mint_a,
         associated_token::authority = escrow,
-        // vault.authority == escrow
-        associated_token::token_program = token_program,
+        token_program = token_program,
     )]
-    pub vault: InterfaceAccount<'info, TokenAccount>,
-    pub token_program: Interface<'info, TokenInterface>,
-    pub system_program: Program<'info, System>, // needed to close the escrow and vault account
-    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub vault: Account<'info, TokenAccount>,
 
+    pub mint_a: InterfaceAccount<'info, Mint>,
+    pub token_program: InterfaceAccount<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
 }
 
-impl<'info> Take<'info> {
-    // transfer the token from the escrow vault ata to the taker
+impl<'info> Release<'info> {
     pub fn release(&mut self) -> Result<()> {
-        let cpi_program = self.token_program.to_account_info();
+        // Seed derivation for PDA authority
+        let seeds = &[
+            b"escrow",
+            self.escrow.maker.as_ref(),
+            &self.escrow.seed.to_le_bytes(),
+            &[self.escrow.bump],
+        ];
+        let signer_seeds = &[&seeds[..]];
 
+        // Transfer tokens from the vault PDA to the receiver ATA
         let cpi_accounts = TransferChecked {
-            from: self.vault.to_account_info(),
-            mint: self.mint_a.to_account_info(),
-            to: self.taker_ata_a.to_account_info(),
+            from:      self.vault.to_account_info(),
+            mint:      self.mint_a.to_account_info(),
+            to:        self.receiver_ata.to_account_info(),
             authority: self.escrow.to_account_info(),
         };
-
-        let seed_bytes = self.escrow.seed.to_le_bytes();
-
-        // this is the original seeds for the escrow account 
-        // [b"escrow", escrow.maker.as_ref(), escrow.seed.to_le_bytes().as_ref()],
-
-        let seeds = &[
-            b"escrow", 
-            self.escrow.maker.as_ref(), 
-            seed_bytes.as_ref(), 
-            &[self.escrow.bump]
-        ]; 
-
-        let signers_seeds = [&seeds[..]];
-
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signers_seeds);
-
-        transfer_checked(cpi_ctx, self.escrow.receive_amount, self.mint_a.decimals)?;
-
+        let cpi_ctx = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            cpi_accounts,
+            signer_seeds,
+        );
+        transfer_checked(cpi_ctx, self.escrow.amount, self.mint_a.decimals)?;
         Ok(())
     }
 
     pub fn close(&mut self) -> Result<()> {
-        // close the escrow account and the vault account here
-        let cpi_program = self.token_program.to_account_info();
+        // Close the vault and escrow PDA just as before
+        let seeds = &[
+            b"escrow",
+            self.escrow.maker.as_ref(),
+            &self.escrow.seed.to_le_bytes(),
+            &[self.escrow.bump],
+        ];
+        let signer_seeds = &[&seeds[..]];
 
-        let cpi_accounts = CloseAccount {
-            account: self.vault.to_account_info(),
+        // Close vault
+        let cpi_close = CloseAccount {
+            account:     self.vault.to_account_info(),
             destination: self.taker.to_account_info(),
-            authority: self.escrow.to_account_info(),
+            authority:   self.escrow.to_account_info(),
         };
-
-        let seed_bytes = self.escrow.seed.to_le_bytes();
-
-        // this seeds needs to match that used in the account struct
-        let seeds = &[b"escrow", self.escrow.maker.as_ref(), seed_bytes.as_ref(), &[self.escrow.bump]];
-
-        let signers_seeds = [&seeds[..]];
-
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signers_seeds);
-
+        let cpi_ctx = CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            cpi_close,
+            signer_seeds,
+        );
         close_account(cpi_ctx)?;
 
+        // Escrow account is closed automatically by `close = maker` in struct
         Ok(())
     }
-
 }
+
 
 #[derive(Accounts)]
 pub struct SetReceiver<'info> {
